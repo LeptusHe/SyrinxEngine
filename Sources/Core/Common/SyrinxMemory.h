38f237550c704e1a61c01b812fd44e82547e2cc9
LeptusHe <heweidong526@gmail.com>
2019-01-17 21:02:38 +0800

    Added macros for allocate and deallocate array

--- Sources/Core/Common/SyrinxMemory.h
+++ Sources/Core/Common/SyrinxMemory.h
@@ -19,8 +19,14 @@ inline void installMemoryLeakDetector()
 
 #if defined(_DEBUG) | !defined(NDEBUG)
     #define SYRINX_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+    #define SYRINX_DELETE delete
+    #define SYRINX_NEW_ARRAY(T, size) new T[size];
+    #define SYRINX_DELETE_ARRAY(ptr)  delete[] ptr 
 #else
     #define SYRINX_NEW new
+    #define SYRINX_DELETE delete
+    #define SYRINX_NEW_ARRAY(T, size) new T[size]
+    #define SYRINX_DELETE_ARRAY(ptr)  delete[] ptr;
 #endif
 
 #endif
