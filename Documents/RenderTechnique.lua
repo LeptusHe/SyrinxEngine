8f323bce0456bc5d38e27656ea40d6c84f6ac2a5
LeptusHe <heweidong526@gmail.com>
2019-05-16 13:50:37 +0800

    Add lua submodule

---
+++ Documents/RenderTechnique.lua
@@ -0,0 +1,15 @@
+function create_render_technique()
+
+
+    render_technique = {
+        name = "forward-shading"
+    }
+    return render_technique
+end
+
+
+
+
+a = create_render_technique()
+print(a.name)
+
